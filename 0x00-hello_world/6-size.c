e9d248781baadfa64a47e24bd12dcffe0095528c
murhangeri <sigidimurha@gmail.com>
2023-06-19 14:49:01 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,4 +10,5 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of float: %d bytes(s)\n", sizeof(float));
+	return (0);
 }
