de3173722039d3100d1c1cd998f09adba18f0006
murhangeri <sigidimurha@gmail.com>
2023-06-20 14:31:09 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * printing
+ */
+void print_alphabet(void)
+{
+	char cAlphabet;
+	for (cAlphabet = 'a';;clphabet = 'z'; cAlphabet ++)
+		_putchar(cAlphabet);
+	_putchar('\n');
+}
