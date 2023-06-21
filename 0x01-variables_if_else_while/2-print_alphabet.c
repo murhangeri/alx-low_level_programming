4158d9a98e725c38597acb3085fb382dc86c980d
murhangeri <sigidimurha@gmail.com>
2023-06-21 16:53:17 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - prints a-z using putchar()
+ * main: prints a-z
+ * return: always zero
+ */
+
+int main(void)
+{
+	char Alphabet;
+
+	for (Alphabet = "a", Alphabet = "z"; Alphabet++)
+	{
+		putchar(Alphabet);
+	}
+	return (0);
+}
