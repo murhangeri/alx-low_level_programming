1414a3c37cd363e8b62a3d58f06841949dda17a3
murhangeri <sigidimurha@gmail.com>
2023-06-21 18:12:31 -0700

    Putchar without char var

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print decimal digits
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	int a = 0;
+	int i = 0;
+	int c = 10
+
+	for (i = 0; i < c; i++)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
