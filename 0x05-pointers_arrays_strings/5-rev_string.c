7e42a1708befe5d82269ab5ffc125286c49d3e3b
murhangeri <sigidimurha@gmail.com>
2023-06-26 15:06:26 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string -reverse string
+ * @s: input
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char rs[];
+	int i;
+	int b = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+	}
+	for (int a = 0; a < i; a++)
+	{
+		i--;
+		rs = s[a];
+		s[a] = s[i];
+	}
+}
