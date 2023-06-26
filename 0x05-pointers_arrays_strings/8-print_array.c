444640e34fd4bf6abbd6561d6f652105c4f403b1
murhangeri <sigidimurha@gmail.com>
2023-06-26 16:18:00 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array- print array
+ * @a: input
+ * @n: input
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int ar[0] = *a;
+	int i = 0;
+
+	while (*s != '\0' && i < n)
+	{
+		printf("%d, ", ar[i]);
+		i++;
+		s++;
+	}
+	_putchar('\n');
+}
