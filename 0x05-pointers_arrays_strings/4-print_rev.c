ba203da4e7bced8de4c3512b774dd3fffff33167
murhangeri <sigidimurha@gmail.com>
2023-06-26 14:10:39 -0700

    Revers string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print revers string
+ * @s: input string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	char *ss;
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		a++;
+	}
+	for (i = a; i > 0; a--)
+	{
+		_putchar(s[i]);
+	}
+}
