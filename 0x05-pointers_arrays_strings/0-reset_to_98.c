ddc09dd345165fe0907f954c3bde5a4bda72c4fd
murhangeri <sigidimurha@gmail.com>
2023-06-26 10:22:39 -0700

    replace by 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset any value to 98
+ * @n - input pinter
+ * Return - returns void
+ */
+
+void reset_to_98(int *n)
+{
+	int *a;
+
+	*a = *n;
+	a = 98;
+	return void;
+}
