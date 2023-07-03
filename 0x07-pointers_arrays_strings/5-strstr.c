fadf0b4ff425710e98725e4311eef162bff9a1b4
murhangeri <sigidimurha@gmail.com>
2023-07-03 16:29:45 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - function
+ * @haystack: input main string
+ * @needle: input searching string
+ *
+ * Return: pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
