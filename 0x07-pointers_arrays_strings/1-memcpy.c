441806256468f3eea2d6c899d3a4daeb77b55be3
murhangeri <sigidimurha@gmail.com>
2023-07-03 14:57:58 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,15 @@
 #include "main.h"
+#include <sring.h>
 /**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @src: input where memory area is copied
+ * @dest: input where memory area is saved
+ * @n: input
  *
+ * Return: return dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (memcpy(dest, src, n));
+}
