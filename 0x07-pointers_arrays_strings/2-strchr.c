8c7c9eb77d96e123931c4636d473151f46239956
murhangeri <sigidimurha@gmail.com>
2023-07-03 15:34:18 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - return c
+ * @s: input
+ * @c: input
+ *
+ * Return: Return c or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
