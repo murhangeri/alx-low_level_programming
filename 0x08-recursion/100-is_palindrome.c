ceff72552133179d5aad48ab3f2d016e65110cd6
murhangeri <sigidimurha@gmail.com>
2023-07-16 07:07:56 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -46,10 +46,11 @@ int palindrome(char *s, int b, int ssize)
 int _strlen_recursion(char *s)
 {
 	int i = 0;
-	
+
 	if (*s)
 	{
-		i++;i += _strlen_recursion(s + 1);
+		i++;
+		i += _strlen_recursion(s + 1);
 	}
 	return (i);
 }
