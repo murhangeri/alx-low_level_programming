9b0df0526cb330cfe76693ca3746c2307eb01a48
murhangeri <sigidimurha@gmail.com>
2023-07-18 16:13:33 -0700

    initializer function for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,6 +10,8 @@ typedef struct dog
 	char *owner;
 	float age;
 }dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
