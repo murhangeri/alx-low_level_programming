5ace6e9d5830acc477f8dd714924d307ee208574
murhangeri <sigidimurha@gmail.com>
2023-07-18 11:57:03 -0700

    Dog struc initiating function

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "dog.h"
-
 /**
- * main - check the code
+ * main - entry point
  *
- * Return: Always 0.
+ * Return: Always zero
  */
+
 int main(void)
 {
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	struct dog my_dog;
+	
+	init_dog(&my_dog, "Poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
 }
