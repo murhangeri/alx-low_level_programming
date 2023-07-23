cda420c7a125a7f042e1c1c834aac4b9a832a408
murhangeri <sigidimurha@gmail.com>
2023-07-23 05:56:17 -0700

    print argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: pointer of array with arguments
+ *
+ * Return: always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+Argc and Argv in C
+Argc is the number of argument including the name of the program that is provided when running the program via command prompt
+Argv[] is a pointer to the array with all the arguments provinded when running the program
+
