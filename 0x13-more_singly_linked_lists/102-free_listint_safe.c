f64b40d368e3ab255a7f25c28e1ef7f978814862
murhangeri <sigidimurha@gmail.com>
2023-07-31 14:56:01 -0700

    extra marks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,7 +7,7 @@
  *
  * Return: pointer or null
  */
-listint_t *listint_loop(listint_t *head)
+listint_t *listint_loop1(listint_t *head)
 {
 	listint_t *node, *last;
 
@@ -42,8 +42,8 @@ size_t free_listint_safe(listint_t **h)
 	if (h == NULL || *h == NULL)
 		return (0);
 
-	loopnode = listint_loop(*h);
-	for (ssize = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
+	loopnode = listint_loop1(*h);
+	for (ssize = 0; (*h != loopnode || i) && *h != NULL; *h = next)
 	{
 		ssize++;
 		next = (*h)->next;
